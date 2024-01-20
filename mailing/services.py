d175bbd8037f2503df9e286e63ff57e65dfeaaec
Aleksandr Buganov <rivares89@gmail.com>
2024-01-20 03:16:19 +0300

    error

--- mailing/services.py
+++ mailing/services.py
@@ -1,5 +1,18 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
 from mailing.models import Mailing
 
 
-def send_letter(sett):
-    item = Mailing.objects.get(pk=sett)
+def send_letter(mailing_item):
+    recipients = mailing_item.recipients.values_list('email', flat=True)
+
+    send_mail(
+        'Рассылочка',
+        str(mailing_item.topic),  # Убедитесь, что тема (topic) представляет собой строку
+        settings.EMAIL_HOST_USER,
+        recipients,
+        fail_silently=False,  # Установите True, если вы не хотите получать исключения при ошибках отправки
+    )
+
+    )
